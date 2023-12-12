2cfd899054b18def438a6d07c7d8036181630cde
Lukas Cornille <lukas.cornille@inthepocket.com>
2023-12-12 15:15:12 +0100

    chore: update packages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,10 @@
-import { type Config } from 'drizzle-kit';
+import { defineConfig } from 'drizzle-kit';
 
-export default {
+export default defineConfig({
   driver: 'better-sqlite',
-  dbCredentials: { url: './app/services/drizzle/db.sqlite' },
+  dbCredentials: {
+    url: './app/services/drizzle/db.sqlite',
+  },
   schema: './app/services/drizzle/schema',
   out: './app/services/drizzle/migrations',
-} satisfies Config;
+});
